849abecdf397c7c45c8c877fc97705cedac8ad23
Jeremy Kemper <jeremy@bitsweat.net>
2007-10-18 18:20:54 +0000

    Increase randomness of TokenGenerator#generate_token by reusing CGI::Session.generate_unique_id. Closes #9622 [rmm5t, Jeremy Kemper]

--- lib/token_generator.rb
+++ lib/token_generator.rb
@@ -1,12 +1,14 @@
 module TokenGenerator
   def generate_token(size = 12, &validity)
+    constant = "#{self.class.name}#{id}"
+
     begin
-      token = Digest::MD5.hexdigest("#{inspect}#{Time.now}").first(size)
+      token = CGI::Session.generate_unique_id(constant)
     end while !validity.call(token) if block_given?
-    
+
     token
   end
-  
+
   def set_token
     self.token = generate_token { |token| self.class.find_by_token(token).nil? }
   end
